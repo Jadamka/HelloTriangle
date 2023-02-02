95b58b87618a98119d625335bf7439da9120b74b
Jan Adamka <59932297+Jadamka@users.noreply.github.com>
2023-02-02 22:22:42 +0100

    Rectangle (Wiremode)

--- HelloTriangle/HelloTriangle/main.cpp
+++ HelloTriangle/HelloTriangle/main.cpp
@@ -49,9 +49,15 @@ int main()
 	}
 
 	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f,
-		0.0, 0.5f, 0.0f,
+		0.5f, 0.5f, 0.0f,	// top right
+		0.5f, -0.5f, 0.0f,	// bottom right
+		-0.5f, -0.5f, 0.0f,	// bottom left
+		-0.5, 0.5f, 0.0f,	// top left
+	};
+
+	unsigned int indices[]= {
+		0, 1, 3,	// first triangle
+		1, 2, 3,	// seconds triangle
 	};
 
 	unsigned int vertexShader;
@@ -98,15 +104,18 @@ int main()
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
-	unsigned int VBO, VAO;
+	unsigned int VBO, VAO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 
 	// bind Vertex Array Object
 	glBindVertexArray(VAO);
 	// copy our vertices array in a buffer for OpenGL to use
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 	// then set the vertex attributes pointers
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
@@ -114,6 +123,8 @@ int main()
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 	glBindVertexArray(0);
+
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);	// drawing in wiremode can be commented out if I want full color rectangle
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -126,7 +137,8 @@ int main()
 		// use our shader program when we want to render an object
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0); // no need to unbind it every frame, can be commented out
 
 		glfwPollEvents();
 		glfwSwapBuffers(window);
